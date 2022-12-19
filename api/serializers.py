a3a3d8171231347ace3a802d569e82f3c6408e17
pparikh <pallav8parikh@gmail.com>
2022-12-20 01:33:59 +0530

    support project details

--- api/serializers.py
+++ api/serializers.py
@@ -2,13 +2,21 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from api.models import TimeLog
+from api.models import TimeLog, Project
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('first_name', 'last_name')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    members = UserSerializer(many=True)
+
+    class Meta:
+        model = Project
+        fields = "__all__"
 
 
 class SideEffectsEvaluationsSerializer(serializers.ModelSerializer):
@@ -19,7 +27,25 @@ class SideEffectsEvaluationsSerializer(serializers.ModelSerializer):
 
 class TimeLogSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=False)
+    project_id = serializers.PrimaryKeyRelatedField(
+        source='project', queryset=Project.objects.all(), write_only=True
+    )
+    project = ProjectSerializer(read_only=True)
+
+    mandatory_project_validation_methods = ('post', 'delete')
 
     class Meta:
         model = TimeLog
         fields = "__all__"
+
+    def validate(self, attrs):
+        request = self.context['request']
+        method = request.method.lower()
+        user = request.user
+        project = attrs.get('project')
+        if (
+            method in self.mandatory_project_validation_methods
+            and not project.members.filter(pk=user.pk).exists()
+        ):
+            raise serializers.ValidationError("user does not belong to project")
+        return attrs
--- time_tracker/settings/local.py
+++ time_tracker/settings/local.py
@@ -1,3 +1,10 @@
 from .common import *
 
 INSTALLED_APPS += ["django_extensions"]
+
+DEFAULT_DATABASE = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
+
+DATABASES = {'default': DEFAULT_DATABASE}
